afdd8706c46fc1ff44314331e3d9212185c213eb
yanli@yellfun.com <Sh123456>
2017-06-10 17:21:17 +0800

    test

--- hotPointCreate.js
+++ hotPointCreate.js
@@ -1,3 +1,29 @@
 /**
  * Created by yan on 09/06/2017.
  */
+var lis = $('ul li')
+
+lis.each(function (index) {
+
+    $(this).click(function () {
+
+        $(this).addClass('active')
+
+        toggleOthers(lis, index)
+    })
+})
+
+function toggleOthers(lis, index) {
+
+    for (var i = 0; i < lis.length; ++i) {
+
+        var ele = $(lis[i])
+
+        if (i == index) {
+
+            continue;
+        }
+
+        ele.removeClass('active')
+    }
+}
